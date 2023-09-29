76bca945cac32df4773119bf55df1f3d792d9669
Shai611 <kabilchaimae1@gmail.com>
2023-09-29 15:44:13 -0700

    commited

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*_print_rev_recursion - Prints a string in reverse.
-*@s: The string to print
-*/
-void _print_rev_recursion(char *s);
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
