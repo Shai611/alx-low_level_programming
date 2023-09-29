026055e20ec3fbaf8e790b504cc528be0f00baa1
Shai611 <kabilchaimae1@gmail.com>
2023-09-29 16:00:14 -0700

    commited

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,14 @@
  */
 int _strlen_recursion(char *s)
 {
-	int n = 0;
+	int longit = 0;
 
 	if (*s)
 	{
-		n++;
-		n= _strlen_recursion(s + 1);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
 
-	return (n);
+	return (longit);
 }
+
